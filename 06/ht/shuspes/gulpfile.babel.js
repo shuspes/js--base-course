5203ab4ca1a691ec8064bb90ab1f6f847ccbc21b
shuspes <shuspes@gmail.com>
2018-01-09 23:48:15 +0300

    Changed public directory (public). Added 'page' gulp task.

--- 06/ht/shuspes/gulpfile.babel.js
+++ 06/ht/shuspes/gulpfile.babel.js
@@ -9,23 +9,28 @@ import uglify from "gulp-uglify";
 
 gulp.task("styles", function() {
   return gulp.src("source/styles/**/*.scss")
-  .on("data", file => console.log(file))  
-  .pipe(sass())
-  .pipe(concat("styles.css"))
-  .pipe(gulp.dest("dest"));
+              .on("data", file => console.log(file))  
+              .pipe(sass())
+              .pipe(concat("styles.css"))
+              .pipe(gulp.dest("public"));
 });
 
 gulp.task("scripts", function() {
   return gulp.src("source/scripts/**/*.js")
-  .on("data", file => console.log(file))
-  .pipe(babel())
-  // .pipe(uglify()) //NOTE: move to build?
-  .pipe(concat("scripts.js"))
-  .pipe(gulp.dest("dest"));
+              .on("data", file => console.log(file))
+              .pipe(babel())
+              // .pipe(uglify()) //NOTE: move to build?
+              .pipe(concat("scripts.js"))
+              .pipe(gulp.dest("public"));
+});
+
+gulp.task("page", function() {
+  return gulp.src("source/index.html")
+              .pipe(gulp.dest("public"));
 });
 
 gulp.task("clean", function() {
-  return del("dest");
+  return del("public");
 });
 
 gulp.task("watch", function() {
@@ -33,6 +38,6 @@ gulp.task("watch", function() {
   gulp.watch("source/styles/**/*.scss", gulp.series("styles"));  
 });
 
-gulp.task("default", gulp.series("clean", gulp.parallel("scripts", "styles")));
+gulp.task("default", gulp.series("clean", "page", gulp.parallel("scripts", "styles")));
 
 gulp.task("dev", gulp.series("default", "watch"));
