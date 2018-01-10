582eb7e41575917fa3e28e49cccd8821da8b5243
shuspes <shuspes@gmail.com>
2018-01-10 22:08:04 +0300

    Updatd favouriteList template. Added styles for List component. Added stub site styles.

--- 06/ht/shuspes/source/scripts/utils/templates.js
+++ 06/ht/shuspes/source/scripts/utils/templates.js
@@ -1,2 +1,9 @@
-export const favouriteItem = "<div class='css-favouriteItem'>{{Name}}</div>";
 export const listContainer = "<div class='css-listContainer'>{{items}}</div>";
+export const favouriteItem = [
+  "<div class='css-favouriteItem'>",
+    "<span class='css-favouriteItem-name'>",
+      "{{Name}}",
+    "</span>",
+  "</div>"
+].join("");
+// export const mainLayout = 
